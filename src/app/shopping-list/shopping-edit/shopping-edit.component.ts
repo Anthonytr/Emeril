73dd3210051173d7c8768a9c19bd4be3701ae126
Anthony Gibson <anthony@trainerroad.com>
2017-06-18 02:15:52 -0700

    Linking and updating recipe list form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms/src/directives';
+import { Component, ElementRef, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -9,21 +10,58 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  // Get access to local varialbe
+  @ViewChild('f') slForm: NgForm;
+
+  subscription: Subscription;
+  editMode: boolean = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    // Check for if user is in "edit mode"
+    this.subscription = this.slService.startedEditing
+      .subscribe(
+        (index: number) => {
+          // Get index item of selected item 
+          this.editedItemIndex = index;
+
+          // Enable "edit mode"
+          this.editMode = true;
+          this.editedItem = this.slService.getIngreditent(index);
+
+          // assingn new values to item
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
 
     // declared in the html for the input "name" attribute
+    // If/else to edit the item or add a new ingredient
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+
+    this.editMode = false;
+
+    //Clear form when done
+    form.reset()
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
